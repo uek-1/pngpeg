4adeebb65da1b25f9de252555a9cc9c42f49f1e7
Vivek <vivek@pop-os.localdomain>
2023-05-24 18:03:25 +0530

    i hate crc algorithms!

--- src/crc.rs
+++ src/crc.rs
@@ -1,7 +1,67 @@
 pub fn png_crc(bytes: Vec<u8>) -> [u8; 4]{
-    let poly = 0xEDB88320u64; //reversed without x^32 term. 
+    let poly = 0x04C11DB7u32; 
+    let mut poly_stack = 0u32;
+    let mut padded_bytes = bytes;
+    padded_bytes.append(&mut vec![0u8;4]);
+    let mut register = 0u32;
     
-    let mut crc = 0xffffffffu64;
+    //Read 4 bytes into register. We just padded it with 4 so the unwrap is safe:
+    let mut shifts = 3;
+    for byte in padded_bytes.clone().into_iter().take(4) {
+        register += (byte as u32) << 8 * shifts;
+        shifts -= 1;
+    }
+
+    //Turn padded_bytes into an iter so we can read from the start:
+    let mut padded_bytes = padded_bytes.into_iter();
+
+    //CRC process :
+    loop {
+        //println!("REGISTER {:#b}", register);
+        //Attempt to read a byte from padded_bytes
+        let next_byte = match padded_bytes.next() {
+            Some(x) => x,
+            None => break,
+        };
+
+        //Read topmost byte:
+        let mut top = register >> 24;
+    
+        //Pop topmost byte from register:
+        register = register << 8;
+
+        //Add next byte to register:
+        register = register + next_byte as u32;
+
+        //Caclulate poly_stack :
+        for i in 0..8 {
+            //println!("poly stack {:#b}", poly_stack);
+            //Read first (MSB) bit of top:
+            top = (top << 1) ^ match (top >> 7) & 1 {
+                //If its a 0, there is no XOR operation
+                0 => 0, 
+                //If its a 1, add poly << 8 to poly_stack because the nth bit of the top byte will
+                //affect 32 - 8 - n bits of the next 4 bytes. 
+                _ => {
+                    poly_stack = poly_stack ^ (poly << (8 - i));
+                    poly
+                },
+            };
+        }
+
+        //XOR register with poly_stack:
+        register = register ^ poly_stack;
+    }
+    println!("CRC: ");
+    for byte in register.clone().to_be_bytes() {
+        print!("{} ", byte);
+    }
+    println!("");
+    //Since the loop broke, register contains only the last four bytes - the remainder
+    register.to_be_bytes()
+}
+/*
+    
     //Create bitstream
     for byte in bytes {
         let mut padded_byte = (crc << 8) + (byte as u64);
@@ -29,6 +89,7 @@ pub fn png_crc(bytes: Vec<u8>) -> [u8; 4]{
 
     [0u8,0u8,0u8,0u8]
 }   
+*/
 
 struct Bits {
     bytes: Vec<u8>,
